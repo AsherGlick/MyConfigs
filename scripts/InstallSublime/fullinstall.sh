976adbf4016abdb0a95e09cc17f28a8861484092
Asher Glick <asher.glick@gmail.com>
2012-12-22 15:39:45 -0500

    sh uses a single equals not a double equals for compare... wierd

--- scripts/InstallSublime/fullinstall.sh
+++ scripts/InstallSublime/fullinstall.sh
@@ -1,17 +1,16 @@
-# 32 bit
-#http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.tar.bz2
-# 64 bit
-#http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64.tar.bz2
-
-
-
 #!/bin/sh
 # This program will download and 'install' sublime text 2 on your linux machine
 
-BITFLAVOR=`uname -m`
-if [ "$BITFLAVOR" == "x86_64" ]; then
-	DOWNLOADLINK="http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64.tar.bz2"
+#BITFLAVOR=`uname -m`
+BITFLAVOR="JJJ"
+BITSIXTYFOUR="x86_65"
+echo "$BITFLAVOR"
+if [ "$BITFLAVOR" = "$BITSIXTYFOUR" ]
+then
+	echo "HELLO WORLD"
+	#DOWNLOADLINK="http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64.tar.bz2"
 else
-	DOWNLOADLINK="http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.tar.bz2"
+	echo "ELSE CASE!"
+	#DOWNLOADLINK="http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.tar.bz2"
 fi
 echo "$DOWNLOADLINK"
