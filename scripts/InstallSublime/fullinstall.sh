5459ec3de8cd3558d5d88abb79886aebb5b59ebb
Asher Glick <asher.glick@gmail.com>
2012-12-22 17:15:25 -0500

    used the mktemp file to download the sublime text tarball

--- scripts/InstallSublime/fullinstall.sh
+++ scripts/InstallSublime/fullinstall.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 # This program will download and 'install' sublime text 2 on your linux machine
 
-#BITFLAVOR=`uname -m`
-BITFLAVOR="JJJ"
-BITSIXTYFOUR="x86_65"
-echo "$BITFLAVOR"
-if [ "$BITFLAVOR" = "$BITSIXTYFOUR" ]
+BITFLAVOR=`uname -m`
+BITSIXTYFOUR="x86_64"
+TEMPFILE=`mktemp`
+#echo "$BITFLAVOR"
+if [ "$BITFLAVOR" = "x86_64" ]
 then
-	echo "HELLO WORLD"
-	#DOWNLOADLINK="http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64.tar.bz2"
+	echo "DETECTED 64bit Operating System"
+	DOWNLOADLINK="http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64.tar.bz2"
 else
-	echo "ELSE CASE!"
-	#DOWNLOADLINK="http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.tar.bz2"
+	echo "DETECTED 32bit Operating System"
+	DOWNLOADLINK="http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.tar.bz2"
 fi
-echo "$DOWNLOADLINK"
+
+#read DOWNLOADLOCATION
+#echo "$DOWNLOADLINK"
+wget -O "$TEMPFILE" "$DOWNLOADLINK"
+echo "EXTRACTING"
+echo "DELEATING TEMP FILES"
+rm "$TEMPFILE"
+echo "CREATING DESKTOP ICON"
+echo "OVERWRITING OLD TEXT EDITOR WITH SUBLIME"
