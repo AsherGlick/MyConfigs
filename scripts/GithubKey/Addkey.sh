ffec202476852b8300cd83b6c2e98ae52aa3f979
Asher Glick <asher.glick@gmail.com>
2012-12-23 00:46:01 -0500

    fixed file finding and fixed the display of variable in the curl command

--- scripts/GithubKey/Addkey.sh
+++ scripts/GithubKey/Addkey.sh
@@ -4,9 +4,9 @@
 # if you dont have a key yet it will prompt you to generate one
 
 # First checkto see if the ssh.pub file exists
-FILE="~/.ssh/id_rsa.pub"
+FILE="$HOME/.ssh/id_rsa.pub"
 
-if [ -f $FILE ];
+if [ -f "$FILE" ];
 then
 	echo "Found Public Key"
 else
@@ -16,10 +16,11 @@ else
 fi
 
 # Grab the title the user would like to use for the key
-read -p "Computer Title" TITLE
+read -p "Computer Title: " TITLE
 # Prompt the user for their github username
-read -p "Github username:" USERNAME
+read -p "Github username: " USERNAME
 # Read the public key into memory
-KEY=`cat ~/.ssh/id_rsa.pub`
+KEY=`cat $HOME/.ssh/id_rsa.pub`
 # send the post request to the github api to add the key
-curl -i -u "$USERNAME" -d '{"title":"$TITLE","key":"$KEY"}' https://api.github.com/user/keys
+#curl -i -u "$USERNAME" -d '{"title":"$TITLE","key":"$KEY"}' https://api.github.com/user/keys
+echo "{\"title\":\"$TITLE\",\"key\":\"$KEY\"}"
