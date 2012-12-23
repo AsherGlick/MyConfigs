d32f3f0348680b0ea4298d4696111d9a85ae1615
Asher Glick <asher.glick@gmail.com>
2012-12-23 00:32:12 -0500

    created a script to try to automaticly add a computers pubkey to github

---
+++ scripts/GithubKey/Addkey.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# this script will add your computer's pubkey to your github account
+# if you dont have a key yet it will prompt you to generate one
+
+# First checkto see if the ssh.pub file exists
+FILE="~/.ssh/id_rsa.pub"
+
+if [ -f $FILE ];
+then
+	echo "Found Public Key"
+else
+	echo "Cannot find public key: generating one for you"
+	read -p "What email would you like the key to have? " EMAIL
+	ssh-keygen -t rsa -C "$EMAIL"
+fi
+
+# Grab the title the user would like to use for the key
+read -p "Computer Title" TITLE
+# Prompt the user for their github username
+read -p "Github username:" USERNAME
+# Read the public key into memory
+KEY=`cat ~/.ssh/id_rsa.pub`
+# send the post request to the github api to add the key
+curl -i -u "$USERNAME" -d '{"title":"$TITLE","key":"$KEY"}' https://api.github.com/user/keys
