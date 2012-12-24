3d6c594e87cbc2500e11122843a30f92e507027b
Asher Glick <asher.glick@gmail.com>
2012-12-24 03:16:03 -0500

    added a bit of text at the top of the full install script

--- scripts/InstallSublime/fullinstall.sh
+++ scripts/InstallSublime/fullinstall.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # This program will download and 'install' sublime text 2 on your linux machine
 
+# this code was written by asher glick and is licenced under the 2-clause bsd licence
+# hopefully it will not stay un commented like this for more the a few more days
+
 # Run the download and extract script
 ./downloadAndExtract.sh
 
