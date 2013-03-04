f9113c0646ed8b57ff03ae91b7e606e2fb044d70
Asher Glick <asher.glick@gmail.com>
2013-03-04 14:26:57 -0500

    added some comments to he new script

--- scripts/ServerSetup/removeDebianCDSource.sh
+++ scripts/ServerSetup/removeDebianCDSource.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+##################################### ABOUT ####################################
+# This script is meant for use when configuring Debian servers, it opens up    #
+# the sources list and comments out the lines that contain sources from the    #
+# CD-ROM, leaving only sources from the net. This enables packages to be       #
+# updated fully. This script may need to be run with root permissions          #
+################################################################################
 
-
+# find all lines that begin with 'deb cdrom:' and replace them with '# deb cdrom:'
 sed -e 's/^deb cdrom:/# deb cdrom:/g' /etc/apt/sources.list > /etc/apt/sources.list
 
 
