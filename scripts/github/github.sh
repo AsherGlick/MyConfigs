cb87ed3aab03e4310c8faab36659f7559d969078
Asher Glick <asher.glick@gmail.com>
2013-05-26 15:26:45 -0400

    fixed a bug where none of the function were being found

--- scripts/github/github.sh
+++ scripts/github/github.sh
@@ -11,13 +11,14 @@ BASEDIR=$(dirname $(readlink -f $0))
 SH=".sh"
 SLASH="/"
 TARGET=$BASEDIR$SLASH$1$SH
-if [ -f TARGET ]
+if [ -f $TARGET ]
 then 
 	shift
 	$TARGET $@
 else 
 	# if the user does not give a valid command run thorugh all of the files
 	# and print out their single line help descriptions
+	echo $TARGET
 	echo "$1 Is not a valid command"
 	for f in $BASEDIR$SLASH*.sh
 	do
