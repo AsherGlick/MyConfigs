4c7678511a843777ae8da6cf504c19b298c89cd8
Asher Glick <asher.glick@gmail.com>
2013-05-26 15:07:12 -0400

    moved all of the github functions to a single folder

---
+++ scripts/github/github.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# The line help function is a single line help file for each script / funciton
+if [ $1 = "--line-help" ]; then
+	echo "github - wrapping function, use this to call all the other functions"
+	exit
+fi
+
+# Actual code
+BASEDIR=$(dirname $(readlink -f $0))
+SH=".sh"
+SLASH="/"
+TARGET=$BASEDIR$SLASH$1$SH
+if [ -f TARGET ]
+then 
+	shift
+	$TARGET $@
+else 
+	# if the user does not give a valid command run thorugh all of the files
+	# and print out their single line help descriptions
+	echo "$1 Is not a valid command"
+	for f in $BASEDIR$SLASH*.sh
+	do
+		$f --line-help
+	done
+fi
